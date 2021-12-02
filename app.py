6a68418516e631f4f5a0fe6d0e10a41a4b89e6d6
jaspervangent69 <57948820+jaspervangent69@users.noreply.github.com>
2021-12-02 18:36:18 +0100

    Added pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! Updated</p>"
+    return "<p>Hello, World! Updated. Added pipeline</p>"
 
 if __name__ == "__main__": 
     app.run(debug=True, host='0.0.0.0')
