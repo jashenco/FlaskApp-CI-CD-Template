97962279659bd9e7b891080c3f65714f5539ac5a
jaspervangent69 <57948820+jaspervangent69@users.noreply.github.com>
2021-12-02 18:42:51 +0100

    Change CI Completed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! Updated. Added pipeline</p>"
+    return "<p>Hello, World! Updated. Added CI pipeline. Let's get started on the CD.</p>"
 
 if __name__ == "__main__": 
     app.run(debug=True, host='0.0.0.0')
