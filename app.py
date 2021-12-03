bded49eec3f48aa069272e9ebdd57111bfdb79e1
jaspervangent69 <57948820+jaspervangent69@users.noreply.github.com>
2021-12-03 16:44:59 +0100

    CD test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>CD testttttt.... Hello, World! Updated. Added CI pipeline. Let's get started on the CD. Oh wait... CD already done. Test 2.</p>"
+    return "<p>Test 4...</p>"
 
 if __name__ == "__main__": 
     app.run(debug=True, host='0.0.0.0')
