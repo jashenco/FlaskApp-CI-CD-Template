c767ce80a82382762bdc7df5e4785f2a055a3032
jaspervangent69 <57948820+jaspervangent69@users.noreply.github.com>
2021-12-04 01:51:33 +0100

    Update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Update in development.</p>"
+    return "<p>Update in development. Damn.</p>"
 
 if __name__ == "__main__": 
     app.run(debug=True, host='0.0.0.0')
